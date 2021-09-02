05f89d7d2e8b764c656835dc6ba6147fd0fdc80b
gomezjesus <jesusgomezbetancourt@MacBook-Pro-de-Jesus.local>
2021-09-01 19:42:07 -0500

    sign in action

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-export default function SigninScreen() {
+import { useDispatch, useSelector } from "react-redux";
+import { signin } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+export default function SigninScreen(props) {
+  const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    //TODO Sign in Action
+    dispatch(signin(email, password));
   };
-
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [userInfo]);
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email address</label>
           <input
